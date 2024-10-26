a9e454a7ff0ca287553aaf7500dba4d052bb4e68
Matthew Wojnicki <mwojnicki@comcast.net>
2024-10-26 18:07:11 -0500

    updated Icon

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,12 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "45.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "Augmenter";
     public const string Author = "LynxC";
 
-    public const string Description = "";
+    public const string Description = "Adds the new Augmenter tower that allows you to purchase augments to improve the capabilities and upgrades of your towers." +
+        "\n\nCurrently Includes:\n - 23 Basic augments\n - 35 Intermediate augments\n - 25 Advanced augments\n - 12 Mastery augments\n - A full quest with a reward upon completion";
 
     public const string RepoOwner = "LynxCGames"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Augmenter-Mod"; // TODO add your repo name here, also in the download url in README.md
